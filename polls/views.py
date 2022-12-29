5c557637421540adcdd6aea53c7c86d986d56fd8
codingMaz <m.balandari.toroghi@gmail.com>
2022-12-29 14:16:18 +0330

    update views for index of polls

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hey guys! Welcome to polls!")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 
 def detail(request, question_id):
